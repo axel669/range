45e329bd2f26de964cf31864c40c7d534c1868aa
axel669 <haryuu.hanekata@gmail.com>
2019-04-24 09:04:06 -0700

    finished tests

--- tests/basic.js
+++ tests/basic.js
@@ -2,36 +2,57 @@ const range = require("../index.js");
 
 describe("Simple ranges", () => {
     describe("from 0", () => {
-        it("to positive", () => {
-            const singleArg = {
-                target: [0, 1, 2, 3, 4],
-                generated: range(5)
-            };
-            const singleArg = {
-                target: [0, 1, 2, 3, 4],
-                generated: range(0, 5)
-            };
+        describe("to positive", () => {
+            it("one arg", () => {
+                expect(range(5))
+                    .toEqual([0, 1, 2, 3, 4]);
+            });
 
-            expect(singleArg.generated)
-                .toEqual(singleArg.target);
-            expect(twoArg.generated)
-                .toEqual(twoArg.target);
+            it("two args", () => {
+                expect(range(0, 5))
+                    .toEqual([0, 1, 2, 3, 4]);
+            });
         });
 
         it("to negative", () => {
-            const twoArg = {
-                const target = [0, -1, -2, -3, -4];
-                const generated = range(0, -5);
-
-            expect(generated)
-                .toEqual(target);
+            expect(range(0, -5))
+                .toEqual([0, -1, -2, -3, -4]);
         });
 
         it("descending", () => {
-            const
+            expect(range(5, 0))
+                .toEqual([5, 4, 3, 2, 1]);
         });
     });
 
-    it("start positive", () => {
+    describe("start positive", () => {
+        it("ascending", () => {
+            expect(range(3, 10))
+                .toEqual([3, 4, 5, 6, 7, 8, 9]);
+        });
+
+        it("descending", () => {
+            expect(range(10, 3))
+                .toEqual([10, 9, 8, 7, 6, 5, 4]);
+        });
+    });
+
+    describe("start negative", () => {
+        describe("ascending", () => {
+            it("to zero", () => {
+                expect(range(-5, 0))
+                    .toEqual([-5, -4, -3, -2, -1]);
+            });
+
+            it("non zero", () => {
+                expect(range(-11, -7))
+                    .toEqual([-11, -10, -9, -8]);
+            });
+        });
+
+        it("descending", () => {
+            expect(range(-3, -7))
+                .toEqual([-3, -4, -5, -6]);
+        });
     });
 });
